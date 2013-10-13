c955aa6a4bc090c7036f14bf6228716f553d3fdb
Chathuranga <chathuranga.hettiarachchi@gmail.com>
2013-10-13 09:54:48 +0530

    Replaced blob lable with track lable, so tracks will be smoothed through time

--- TrackingPlugin/package_tracking/BlobTracking.cpp
+++ TrackingPlugin/package_tracking/BlobTracking.cpp
@@ -34,74 +34,83 @@ const cvb::CvTracks BlobTracking::getTracks()
 
 void BlobTracking::process(const cv::Mat &img_input, const cv::Mat &img_mask, cv::Mat &img_output)
 {
-  if(img_input.empty() || img_mask.empty())
-    return;
+    if(img_input.empty() || img_mask.empty()){
+        return;
+    }
+    loadConfig();
 
-  loadConfig();
+    if(firstTime){
+        saveConfig();
+    }
+    IplImage* frame = new IplImage(img_input);
+    cvConvertScale(frame, frame, 1, 0);
 
-  if(firstTime){
+    IplImage* segmentated = new IplImage(img_mask);
 
-    saveConfig();
-	
-  }
-  IplImage* frame = new IplImage(img_input);
-  cvConvertScale(frame, frame, 1, 0);
+    IplConvKernel* morphKernel = cvCreateStructuringElementEx(5, 5, 1, 1, CV_SHAPE_RECT, NULL);
+    cvMorphologyEx(segmentated, segmentated, NULL, morphKernel, CV_MOP_OPEN, 1);
 
-  IplImage* segmentated = new IplImage(img_mask);
-  
-  IplConvKernel* morphKernel = cvCreateStructuringElementEx(5, 5, 1, 1, CV_SHAPE_RECT, NULL);
-  cvMorphologyEx(segmentated, segmentated, NULL, morphKernel, CV_MOP_OPEN, 1);
+    //if(showBlobMask)
+    //  cvShowImage("Blob Mask", segmentated);
 
-  //if(showBlobMask)
-  //  cvShowImage("Blob Mask", segmentated);
+    IplImage* labelImg = cvCreateImage(cvGetSize(frame), IPL_DEPTH_LABEL, 1);
 
-  IplImage* labelImg = cvCreateImage(cvGetSize(frame), IPL_DEPTH_LABEL, 1);
+    cvb::CvBlobs blobs;
+    unsigned int result = cvb::cvLabel(segmentated, labelImg, blobs);
 
-  cvb::CvBlobs blobs;
-  unsigned int result = cvb::cvLabel(segmentated, labelImg, blobs);
-  
-  //cvb::cvFilterByArea(blobs, 500, 1000000);
-  cvb::cvFilterByArea(blobs, minArea, maxArea);
-  
-  QTextStream out_stream(&file);
-
-  for(std::map<cvb::CvLabel,cvb::CvBlob *>::iterator it = blobs.begin() ; it != blobs.end(); it++){
-  
-	cvb::CvLabel lablel = (*it).first;
-    cvb::CvBlob * blob = (*it).second;
-	//out_stream << blob;
-	out_stream << frameIndex << "," << lablel  << ","<< blob->centroid.x << "," << blob->centroid.y << "|";
-
-  }
-	out_stream << "\n";
-
+    //cvb::cvFilterByArea(blobs, 500, 1000000);
+    cvb::cvFilterByArea(blobs, minArea, maxArea);
   
 
-  //cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX);
-  if(debugBlob)
-    cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX|CV_BLOB_RENDER_CENTROID|CV_BLOB_RENDER_ANGLE|CV_BLOB_RENDER_TO_STD);
-  else
-    cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX|CV_BLOB_RENDER_CENTROID|CV_BLOB_RENDER_ANGLE);
-
-  cvb::cvUpdateTracks(blobs, tracks, 200., 5);
-  
-    unsigned int j=0;
-    for (cvb::CvTracks::const_iterator track = tracks.begin(); track!=tracks.end(); ++track, j++)
-    {
-        if((*track).second->active != 0){
-
-            std::cout  << "Active track: " << (*track).first << "\n";
-        }
-
+    //cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX);
+    if(debugBlob){
+        cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX|CV_BLOB_RENDER_CENTROID|CV_BLOB_RENDER_ANGLE|CV_BLOB_RENDER_TO_STD);
+    }
+    else{
+        cvb::cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX|CV_BLOB_RENDER_CENTROID|CV_BLOB_RENDER_ANGLE);
     }
 
+    cvb::cvUpdateTracks(blobs, tracks, 20.0, 5);
+
+    QTextStream out_stream(&file);
 
 
-  if(debugTrack)
-    cvb::cvRenderTracks(tracks, frame, frame, CV_TRACK_RENDER_ID|CV_TRACK_RENDER_BOUNDING_BOX|CV_TRACK_RENDER_TO_STD);
-  else
-    cvb::cvRenderTracks(tracks, frame, frame, CV_TRACK_RENDER_ID|CV_TRACK_RENDER_BOUNDING_BOX);
-  
+    //At this point, we have assigned each blob in current frame in to a track. so we can iterate through active tracks, and
+    // find out out blobs within one of those tracks. Following loop does that. This is helpfull to have more generalized idea about
+    // relationship between blobs with increasing time.
+
+    for (cvb::CvTracks::const_iterator track = tracks.begin(); track!=tracks.end(); ++track)
+    {
+        if((*track).second->active != 0){
+            //std::cout  << "Active track: " << (*track).first << "\n";
+
+            for(std::map<cvb::CvLabel,cvb::CvBlob *>::iterator it = blobs.begin() ; it != blobs.end(); it++){
+
+              cvb::CvLabel label = (*it).first;
+              cvb::CvBlob * blob = (*it).second;
+              //out_stream << blob;
+
+              if((*track).second->label == label){
+                //This is fine grained individual blob lables
+                //out_stream << frameIndex << "," << label  << ","<< blob->centroid.x << "," << blob->centroid.y <<  "|";
+
+
+                //This is smoothed time tracked blob lables
+                  out_stream << frameIndex << "," << (*track).first  << ","<< blob->centroid.x << "," << blob->centroid.y <<  "|";
+
+              }
+            }
+        }
+    }
+
+    out_stream << "\n";
+
+    if(debugTrack){
+        cvb::cvRenderTracks(tracks, frame, frame, CV_TRACK_RENDER_ID|CV_TRACK_RENDER_BOUNDING_BOX|CV_TRACK_RENDER_TO_STD);
+    }
+    else{
+        cvb::cvRenderTracks(tracks, frame, frame, CV_TRACK_RENDER_ID|CV_TRACK_RENDER_BOUNDING_BOX);
+    }
 
   //std::map<CvID, CvTrack *> CvTracks
 
